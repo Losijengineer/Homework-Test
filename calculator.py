fc2825a4fb49b8d727db4d94d43c33dbc8f32852
Losijengineer <37987960+Losijengineer@users.noreply.github.com>
2022-09-02 18:58:16 +0800

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,4 @@
+﻿def add(a, b):
+    return a + b
+def minus(a, b):
+    return a - b
---
+++ test_calculator.py
@@ -0,0 +1,7 @@
+﻿import calculator
+def test_add():
+    ret = calculator.add(114, 514)
+    assert(ret == 628)
+def test_minus():
+    ret = calculator.minus(114, 514)
+    assert(ret == -400)
